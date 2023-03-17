991f322f495ba456ce1bd847dff50a3a15fda219
waisimon <waisimon@outlook.com>
2023-03-18 00:37:33 +0800

    feat: init

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,38 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+export default function () {
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+    const companyName = 'Smile Chain'
+    return <>
+        <Head>
+            <title>{companyName}</title>
+            <link rel="icon" href="/favicon.ico" />
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Coming Soon - {companyName}</title>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.4/tailwind.min.css" />
+        </Head>
+        <header class="py-10">
+            <div class="container mx-auto flex justify-center items-center flex-col">
+                <h1 class="text-6xl text-gray-900 font-bold mb-2">{companyName}</h1>
+                <p class="text-lg text-gray-800 mb-10">We're launching soon! Stay tuned for updates.</p>
+                <div class="flex py-6">
+                    <input type="text" class="px-4 py-2 border border-gray-400 rounded-l w-full" placeholder="Enter your email address" />
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r">Subscribe</button>
+                </div>
+                <p class="text-sm text-gray-600">Don't worry, we hate spam too!</p>
+            </div>
+        </header>
+        <footer class="bg-gray-800 text-gray-100 py-10">
+            <div class="container mx-auto flex justify-between items-center flex-wrap">
+                <p class="mb-2">&copy; {(new Date().getFullYear())} {companyName}. All rights reserved.</p>
+                <div class="flex items-center">
+                    <a href="#" class="mx-3 hover:text-gray-400"><i class="fab fa-twitter" /></a>
+                    <a href="#" class="mx-3 hover:text-gray-400"><i class="fab fa-facebook" /></a>
+                    <a href="#" class="mx-3 hover:text-gray-400"><i class="fab fa-instagram" /></a>
+                </div>
+            </div>
+        </footer>
+    </>
 }
